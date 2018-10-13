fdc8b690bf95b0acfcb229d128b1760bc76f65d5
Elena <ees.elena@gmail.com>
2018-10-13 17:32:20 +0300

    task_1 выполнено
    добавлено умножение массива массивов

--- task_1/MatrixMult/MatrixMult/Program.cs
+++ task_1/MatrixMult/MatrixMult/Program.cs
@@ -8,12 +8,15 @@ namespace MatrixMult
     {
         static void Main(string[] args)
         {
-            int m, n, k, count;
+            int m, n, k, count, type;
+
+            Console.Write("Choose type of arrays: 1 - array2D ;  2 - jaggedArray \r\n>");
+            type = Int32.Parse(Console.ReadLine());
 
             Console.Write("Enter matrixes' size (M, N, K) where the first one is M x N size and the second one is N x K size \r\n>");
             var lineSize = Console.ReadLine();
 
-            int[] numbers = lineSize?.Split(',')?.Where(s => string.IsNullOrWhiteSpace(s) == false)?.Select(s => int.Parse(s.Trim()))?.ToArray();
+            int[] numbers = lineSize?.Split(',')?.Where(s => !string.IsNullOrWhiteSpace(s))?.Select(s => int.Parse(s.Trim()))?.ToArray();
             if (numbers == null || numbers.Length != 3)
             {
                 Console.WriteLine("Incorrect parameters count");
@@ -23,47 +26,87 @@ namespace MatrixMult
             m = numbers[0];
             n = numbers[1];
             k = numbers[2];
-            if (m <= 0 || n <= 0 || k <= 0)
-            {
-                Console.WriteLine("Incorrect parameters");
-                Console.ReadKey();
-                return;
-            }
+
             Console.Write("Enter iteration quantity \r\n>");
             var lineCount = Console.ReadLine();
             count = int.Parse(lineCount);
 
-            var statistics = new long[count];
+
             var sw = new Stopwatch();
 
-            for (int i = 0; i < count; i++)
+            if (type == 1)
             {
-                var left = GenerateMatrix(m, n);
-                var right = GenerateMatrix(n, k);
+                var statistics = new long[count];
+                for (int i = 0; i < count; i++)
+                {
+                    var left = GenerateMatrix(m, n);
+                    var right = GenerateMatrix(n, k);
 
-                sw.Start();
-                Multiply(left, right);
-                sw.Stop();
-                statistics[i] = sw.ElapsedMilliseconds;
-                sw.Reset();
+                    sw.Start();
+                    Multiply(left, right);
+                    sw.Stop();
+                    statistics[i] = sw.ElapsedMilliseconds;
+                    sw.Reset();
+                }
+                var avg = statistics.Sum() / count;
+
+                Console.WriteLine($"Average time: {avg} ms");
+                Console.WriteLine($"Capacity: {2.0d * m * n * k / avg}");
+
+                Console.ReadKey();
+                return;
             }
-            var avg = statistics.Sum() / count;
+            if (type == 2)
+            {
+                var statistics_jag = new long[count];
+                for (int i = 0; i < count; i++)
+                {
+                    var left = GenerateJaggedArray(m, n);
+                    var right = GenerateJaggedArray(n, k);
 
-            Console.WriteLine($"Average time: {avg} ms");
-            Console.WriteLine($"Capacity: {2.0d * m * n * k / avg}");
+                    sw.Start();
+                    MultiplyJaggedArray(left, right);
+                    sw.Stop();
+                    statistics_jag[i] = sw.ElapsedMilliseconds;
+                    sw.Reset();
+                }
+                var avg_jag = statistics_jag.Sum() / count;
 
-            Console.ReadKey();
+                Console.WriteLine($"Average time: {avg_jag} ms");
+                Console.WriteLine($"Capacity: {2.0d * m * n * k / avg_jag}");
+
+                Console.ReadKey();
+                return;
+            }
+
+
+
         }
 
-
-        private static double[,] GenerateMatrix(int m, int n) {
-            var matrix = new double[m,n];
+        private static double[,] GenerateMatrix(int m, int n)
+        {
+            var matrix = new double[m, n];
             var rnd = new Random((int)Environment.TickCount);
             for (int i = 0; i < m; i++)
             {
                 for (int j = 0; j < n; j++)
                 {
                     matrix[i, j] = rnd.NextDouble();
+                }
+            }
+            return matrix;
+        }
+
+        private static double[][] GenerateJaggedArray(int m, int n)
+        {
+            var matrix = new double[m][];
+            var rnd = new Random((int)Environment.TickCount);
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                matrix[i] = new double[n];
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    matrix[i][j] = rnd.NextDouble();
                 }
             }
             return matrix;
@@ -76,10 +119,27 @@ namespace MatrixMult
             {
                 for (int j = 0; j < resultMatrix.GetLength(1); j++)
                 {
-                    resultMatrix[i, j] = 0;
-                    for (int k = 0; k < matrix_1.GetLength(0); k++)
+                  //  resultMatrix[i, j] = 0;
+                    for (int k = 0; k < matrix_1.GetLength(1); k++)
                     {
                         resultMatrix[i, j] += matrix_1[i, k] * matrix_2[k, j];
+                    }
+                }
+            }
+        }
+        private static void MultiplyJaggedArray(double[][] matrix_1, double[][] matrix_2)
+        {
+            var resultMatrix = new double[matrix_1.GetLength(0)][];
+            // Console.Write(matrix_2[0].GetLength(0));
+            for (int i = 0; i < resultMatrix.GetLength(0); i++)
+            {
+                resultMatrix[i] = new double[matrix_2[0].GetLength(0)];
+
+                for (int j = 0; j < resultMatrix[i].Length; j++)
+                {
+                    for (int k = 0; k < resultMatrix[i].Length; k++)
+                    {
+                        resultMatrix[i][j] += matrix_1[i][k] * matrix_2[k][j];
                     }
                 }
             }
